276a655f7e55f6051c55274b7b54cf9f17c6a8e3
renegade47 <leoruther66@gmail.com>
2017-12-31 20:26:36 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+Plugin Name: Editor Stylify
+Plugin URI: 
+Description: This plugin adds a custom editor stylesheet for the WordPress Editor
+Author: Leo Ruther Valles
+Version: 1
+Author URI: http://leoruthervalles.lineage-developers.com
+*/
+
+
+function register_editor_stylesheet() {
+    add_editor_style( 'css/es_customeditor.css' );
+}
+add_action( 'admin_init', 'register_editor_stylesheet' );
+
+
+?>
